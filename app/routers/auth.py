fd69191348d2f68a47eec7bf9242676f81849cb1
Angus Rausch <angus@rausch.com.au>
2026-08-15 20:26:59 +1000

    Moved templates discovery into seperate file
    Created `templates.py` to manage getting templates

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -10,14 +10,10 @@ from app.utils.sanitise import sanitize_username
 from app.core.config import settings
 from app.core.security import ALGORITHM, COOKIE_NAME
 from app.core.errors import InvalidCredentialsError
+from app.core.templates import templates
 
 router = APIRouter()
 
-# Dynamically finds the absolute path to simple-zfs-manager/app/templates
-base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-templates_dir = os.path.join(base_dir, "templates")
-
-templates = Jinja2Templates(directory=templates_dir)
 
 @router.get("/login")
 async def read_login(request: Request):
--- app/routers/dashboard.py
+++ app/routers/dashboard.py
@@ -1,14 +1,11 @@
 import os
 from fastapi import APIRouter, Request, Depends
 from fastapi.templating import Jinja2Templates
+
 from app.core.security import get_current_user
+from app.core.templates import templates
+
 router = APIRouter()
-
-# Dynamically finds the absolute path to simple-zfs-manager/app/templates
-base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-templates_dir = os.path.join(base_dir, "templates")
-
-templates = Jinja2Templates(directory=templates_dir)
 
 @router.get("/")
 async def read_home(request: Request, uid: int = Depends(get_current_user)):
