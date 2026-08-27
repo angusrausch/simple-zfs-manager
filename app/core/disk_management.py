b96869c236f0ab5e4b86b33a10113eb4bd10a474
Angus Rausch <angus@rausch.com.au>
2026-08-28 08:01:10 +1000

    Added tests for disk_management
    Testing returns from different commands

--- app/core/disk_management.py
+++ app/core/disk_management.py
@@ -4,7 +4,7 @@ from app.core.system.runner import run_command
 from app.core.zfs.zpool import get_used_disks
 
 
-def list_disks(uid):
+def list_disks(uid: int) -> list[dict]:
     def _is_mounted(disk):
         return disk["mountpoint"] is not None or any(
             child["mountpoint"] is not None for child in disk.get("children", [])
@@ -20,12 +20,12 @@ def list_disks(uid):
     ]
 
 
-async def list_unused_disks(uid):
+async def list_unused_disks(uid: int) -> list[dict]:
     used_disks = await get_used_disks(uid)
     return [disk for disk in list_disks(uid) if not disk["mounted"] and disk["name"] not in used_disks]
 
 
-def _get_disk_json(uid):
+def _get_disk_json(uid: int) -> dict:
     command = ["lsblk", "-b", "--json", "-o", "name,size,mountpoint"]
 
     status, return_str = run_command(uid, command)
---
+++ tests/core/test_returns/test_list_unused_disks.py
@@ -0,0 +1,10 @@
+value = [
+    [
+        {'name': 'sda', 'size': 240057409536, 'mounted': False}, 
+        {'name': 'sdb', 'size': 240057409536, 'mounted': False}, 
+        {'name': 'sdc', 'size': 250059350016, 'mounted': False}, 
+        {'name': 'sdd', 'size': 250059350016, 'mounted': False}, 
+        {'name': 'mmcblk0', 'size': 127865454592, 'mounted': True}
+    ],
+    ['raidz1-0', 'sda', 'sdb']
+]
